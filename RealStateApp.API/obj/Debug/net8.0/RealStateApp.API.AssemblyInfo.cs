b65bc57ae46317eaf7090d96c7e05ff9c8688544
abawzr <145490935+abawzr@users.noreply.github.com>
2025-02-06 18:50:15 +0300

    commit

--- RealStateApp.API/obj/Debug/net8.0/RealStateApp.API.AssemblyInfo.cs
+++ RealStateApp.API/obj/Debug/net8.0/RealStateApp.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealStateApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0128c0db3074d5504611719b6cbe9eab60f5b590")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealStateApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealStateApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
